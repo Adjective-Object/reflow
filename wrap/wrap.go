170010c71ddc2a25006402e1faeaf5273aed4ea9
Max Huang-Hobbs <Maxwell.HuangHobbs@microsoft.com>
2024-06-03 17:58:54 +0000

    wrap: add wrap.WrapHeight
    Adds a new writer to compute the height of wrapped text without actually
    allocating & wrapping the text. This is for use when computing layout in
    an ansi terminal in an environment with a constrained height.

--- wrap/wrap.go
+++ wrap/wrap.go
@@ -2,11 +2,11 @@ package wrap
 
 import (
 	"bytes"
-	"strings"
 	"unicode"
+	"unicode/utf8"
 
 	"github.com/mattn/go-runewidth"
-	"github.com/muesli/reflow/ansi"
+	"github.com/muesli/reflow/internal/statemachine"
 )
 
 var (
@@ -23,7 +23,7 @@ type Wrap struct {
 
 	buf             *bytes.Buffer
 	lineLen         int
-	ansi            bool
+	state           statemachine.StateMachine
 	forcefulNewline bool
 }
 
@@ -54,64 +54,125 @@ func Bytes(b []byte, limit int) []byte {
 	return f.Bytes()
 }
 
-func (w *Wrap) addNewLine() {
-	_, _ = w.buf.WriteRune('\n')
-	w.lineLen = 0
-}
-
 // String is shorthand for declaring a new default Wrap instance,
 // used to immediately wrap a string.
 func String(s string, limit int) string {
 	return string(Bytes([]byte(s), limit))
 }
 
+func (w *Wrap) addNewLine(c rune) {
+	// fmt.Println("  newline")
+	_, _ = w.buf.WriteRune(c)
+	w.lineLen = 0
+}
+
 func (w *Wrap) Write(b []byte) (int, error) {
-	s := strings.Replace(string(b), "\t", strings.Repeat(" ", w.TabWidth), -1)
-	if !w.KeepNewlines {
-		s = strings.Replace(s, "\n", "", -1)
+	// guard against negative TabWidth
+	if w.TabWidth < 0 {
+		w.TabWidth = 0
 	}
 
-	width := ansi.PrintableRuneWidth(s)
-
-	if w.Limit <= 0 || w.lineLen+width <= w.Limit {
-		w.lineLen += width
+	if w.Limit <= 0 {
+		// pass-through if no limit is set
 		return w.buf.Write(b)
 	}
 
-	for _, c := range s {
-		if c == ansi.Marker {
-			w.ansi = true
-		} else if w.ansi {
-			if ansi.IsTerminator(c) {
-				w.ansi = false
-			}
-		} else if inGroup(w.Newline, c) {
-			w.addNewLine()
-			w.forcefulNewline = false
-			continue
-		} else {
-			width := runewidth.RuneWidth(c)
-
-			if w.lineLen+width > w.Limit {
-				w.addNewLine()
-				w.forcefulNewline = true
-			}
-
-			if w.lineLen == 0 {
-				if w.forcefulNewline && !w.PreserveSpace && unicode.IsSpace(c) {
-					continue
-				}
-			} else {
-				w.forcefulNewline = false
-			}
-
-			w.lineLen += width
+	i := 0
+	for i < len(b) {
+		// iterate runewise over the input byte slice w/o allocating
+		c, cw := utf8.DecodeRune(b[i:])
+		var step statemachine.StateTransition
+		nextI := i + cw
+		for j := i; j < nextI; j++ {
+			step = w.state.Next(b[j])
 		}
+		i = nextI
 
-		_, _ = w.buf.WriteRune(c)
+		w.stepState(step, c)
 	}
 
 	return len(b), nil
+}
+
+func (w *Wrap) WriteString(s string) (int, error) {
+	// guard against negative TabWidth
+	if w.TabWidth < 0 {
+		w.TabWidth = 0
+	}
+
+	if w.Limit <= 0 {
+		// pass-through if no limit is set
+		return w.buf.WriteString(s)
+	}
+
+	// iterate the bytes of the string, without allocating
+	for i := 0; i < len(s); {
+		c, charWidth := utf8.DecodeRuneInString(s[i:])
+		nextI := i + charWidth
+
+		// update state machine by stepping over the current character, one byte at a time
+		var step statemachine.StateTransition
+		for j := i; j < nextI; j++ {
+			step = w.state.Next(s[j])
+		}
+
+		w.stepState(step, c)
+		i = nextI
+	}
+
+	return len(s), nil
+}
+
+func (w *Wrap) stepState(step statemachine.StateTransition, c rune) {
+	if !step.IsPrinting() {
+		// echo through non-printable characters
+		_, _ = w.buf.WriteRune(c)
+		return
+	}
+
+	if inGroup(w.Newline, c) && !w.KeepNewlines {
+		return
+	}
+
+	if c == '\t' {
+		for i := 0; i < w.TabWidth; i++ {
+			w.stepPrintableState(' ')
+		}
+	} else {
+		w.stepPrintableState(c)
+	}
+}
+
+func (w *Wrap) stepPrintableState(c rune) {
+	// fmt.Println("stepPrintableState\t", strconv.Quote(string(c)))
+
+	if inGroup(w.Newline, c) {
+		w.addNewLine(c)
+		w.forcefulNewline = false
+		return
+	} else {
+		width := runewidth.RuneWidth(c)
+
+		if !w.PreserveSpace &&
+			((w.lineLen == 0 && w.forcefulNewline) || w.lineLen+width > w.Limit) &&
+			unicode.IsSpace(c) {
+			// skip leading whitespaces on a new line if PreserveSpace == false
+			// also skip forcing a newline if the next line would be only a stripped whitespace.
+			return
+		}
+
+		if w.lineLen > 0 && w.lineLen+width > w.Limit {
+			w.addNewLine('\n')
+			w.forcefulNewline = true
+		} else {
+			// clear forceful newline flag if we didn't just force a newline
+			w.forcefulNewline = false
+		}
+
+		w.lineLen += width
+	}
+
+	_, _ = w.buf.WriteRune(c)
 }
 
 // Bytes returns the wrapped result as a byte slice.
@@ -131,4 +192,165 @@ func inGroup(a []rune, c rune) bool {
 		}
 	}
 	return false
+}
+
+// Limited version of wordwrap.Wrap that only counts
+// the height of the wrapped message in newlines
+type WrapHeight struct {
+	Limit         int
+	KeepNewlines  bool
+	PreserveSpace bool
+	TabWidth      int
+	Newline       []rune
+
+	// Tracks if the last newline was "forced" (e.g. was inserted due to
+	// the line-length limit)
+	//
+	// This is because `PreserveSpace` is only applied after forced
+	// newlines
+	forcefulNewline bool
+	ansiState       statemachine.StateMachine
+	height          int
+	lineLen         int // current length of the current line
+}
+
+func (h WrapHeight) Height() int {
+	return h.height + 1
+}
+
+func (w *WrapHeight) Write(b []byte) (int, error) {
+	// parse the input byte slice as runes, without allocating
+	i := 0
+	for i < len(b) {
+		c, byteWidth := utf8.DecodeRune(b[i:])
+		nextI := i + byteWidth
+
+		// consume all the bytes of this character in the statemachine
+		var step statemachine.StateTransition
+		for j := i; j < nextI; j++ {
+			step = w.ansiState.Next(b[j])
+		}
+
+		w.stepState(step, c)
+		i = nextI
+	}
+
+	return len(b), nil
+}
+
+func (w *WrapHeight) WriteString(s string) (int, error) {
+	// iterate the bytes of the string, without allocating
+	for i := 0; i < len(s); {
+		c, charWidth := utf8.DecodeRuneInString(s[i:])
+		nextI := i + charWidth
+
+		// update state machine by stepping over the current character, one byte at a time
+		var step statemachine.StateTransition
+		for j := i; j < nextI; j++ {
+			step = w.ansiState.Next(s[j])
+		}
+
+		w.stepState(step, c)
+		i = nextI
+	}
+
+	return len(s), nil
+}
+
+func (w *WrapHeight) stepState(step statemachine.StateTransition, c rune) {
+	// fmt.Println("stepState\t", strconv.Quote(string(c)), w.height)
+
+	if w.Limit <= 0 {
+		// if limit <=0, ignore all special behaviour and just
+		// count the printable newlines
+		if inGroup(w.Newline, c) && step.IsPrinting() {
+			w.height++
+		}
+		return
+	}
+
+	// special case: intercept tabs and treat them as some number of spaces
+	if c == '\t' {
+		for i := 0; i < w.TabWidth; i++ {
+			w.stepState(step, ' ')
+		}
+		return
+	}
+
+	// if we are not printing, totally ignore the character for the stepper calculation
+	if !step.IsPrinting() {
+		return
+	}
+
+	// special case: if !KeepNewlines, drop newlines
+	if inGroup(w.Newline, c) {
+		if !w.KeepNewlines {
+			// if not in KeepNewlines,
+			// skip newline characters
+			return
+		} else {
+			w.forcefulNewline = false
+			// Otherwise, consume this newline
+			w.height++
+			w.lineLen = 0
+			return
+		}
+	}
+
+	width := runewidth.RuneWidth(c)
+
+	if !w.PreserveSpace &&
+		((w.lineLen == 0 && w.forcefulNewline) || w.lineLen+width > w.Limit) &&
+		unicode.IsSpace(c) {
+		// skip leading whitespaces on a new line if PreserveSpace == false
+		// also skip forcing a newline if the next line would be only a stripped whitespace.
+		return
+	}
+
+	if w.lineLen > 0 && w.lineLen+width > w.Limit {
+		// end of current line
+		w.height++
+		w.forcefulNewline = true
+
+		// This effectively pre-buffers a line break for the next line, which matches
+		// the real implementation. We do this to avoid
+		w.lineLen = 0
+	} else {
+		// clear forceful newline flag if we didn't just force a newline
+		w.forcefulNewline = false
+	}
+
+	w.lineLen += width
+}
+
+func NewHeightWriter(limit int) *WrapHeight {
+	return &WrapHeight{
+		Limit:         limit,
+		Newline:       defaultNewline,
+		KeepNewlines:  true,
+		PreserveSpace: false,
+		TabWidth:      defaultTabWidth,
+	}
+}
+
+func Height(s string, limit int) int {
+	writer := WrapHeight{
+		Limit:    limit,
+		Newline:  defaultNewline,
+		TabWidth: defaultTabWidth,
+	}
+	writer.WriteString(s)
+
+	return writer.Height()
+}
+
+func HeightBytes(b []byte, limit int) int {
+	writer := WrapHeight{
+		Limit:    limit,
+		Newline:  defaultNewline,
+		TabWidth: defaultTabWidth,
+	}
+	writer.Write(b)
+
+	return writer.Height()
 }
--- wrap/wrap_test.go
+++ wrap/wrap_test.go
@@ -2,119 +2,135 @@ package wrap
 
 import (
 	"bytes"
+	"strconv"
+	"strings"
 	"testing"
+
+	"github.com/muesli/reflow/internal/statemachine"
 )
 
-func TestWrap(t *testing.T) {
-	tt := []struct {
-		Input         string
-		Expected      string
-		Limit         int
-		KeepNewlines  bool
-		PreserveSpace bool
-		TabWidth      int
-	}{
-		// No-op, should pass through, including trailing whitespace:
-		{
-			Input:         "foobar\n ",
-			Expected:      "foobar\n ",
-			Limit:         0,
-			KeepNewlines:  true,
-			PreserveSpace: false,
-			TabWidth:      0,
-		},
-		// Nothing to wrap here, should pass through:
-		{
-			Input:         "foo",
-			Expected:      "foo",
-			Limit:         4,
-			KeepNewlines:  true,
-			PreserveSpace: false,
-			TabWidth:      0,
-		},
-		// In contrast to wordwrap we break a long word to obey the given limit
-		{
-			Input:         "foobarfoo",
-			Expected:      "foob\narfo\no",
-			Limit:         4,
-			KeepNewlines:  true,
-			PreserveSpace: false,
-			TabWidth:      0,
-		},
-		// Newlines in the input are respected if desired
-		{
-			Input:         "f\no\nobar",
-			Expected:      "f\no\noba\nr",
-			Limit:         3,
-			KeepNewlines:  true,
-			PreserveSpace: false,
-			TabWidth:      0,
-		},
-		// Newlines in the input can be ignored if desired
-		{
-			Input:         "f\no\nobar",
-			Expected:      "foo\nbar",
-			Limit:         3,
-			KeepNewlines:  false,
-			PreserveSpace: false,
-			TabWidth:      0,
-		},
-		// Leading whitespaces after forceful line break can be preserved if desired
-		{
-			Input:         "foo bar\n  baz",
-			Expected:      "foo\n ba\nr\n  b\naz",
-			Limit:         3,
-			KeepNewlines:  true,
-			PreserveSpace: true,
-			TabWidth:      0,
-		},
-		// Leading whitespaces after forceful line break can be removed if desired
-		{
-			Input:         "foo bar\n  baz",
-			Expected:      "foo\nbar\n  b\naz",
-			Limit:         3,
-			KeepNewlines:  true,
-			PreserveSpace: false,
-			TabWidth:      0,
-		},
-		// Tabs are broken up according to the configured TabWidth
-		{
-			Input:         "foo\tbar",
-			Expected:      "foo \n  ba\nr",
-			Limit:         4,
-			KeepNewlines:  true,
-			PreserveSpace: true,
-			TabWidth:      3,
-		},
-		// Remaining width of wrapped tab is ignored when space is not preserved
-		{
-			Input:         "foo\tbar",
-			Expected:      "foo \nbar",
-			Limit:         4,
-			KeepNewlines:  true,
-			PreserveSpace: false,
-			TabWidth:      3,
-		},
-		// ANSI sequence codes don't affect length calculation:
-		{
-			Input:         "\x1B[38;2;249;38;114mfoo\x1B[0m\x1B[38;2;248;248;242m \x1B[0m\x1B[38;2;230;219;116mbar\x1B[0m",
-			Expected:      "\x1B[38;2;249;38;114mfoo\x1B[0m\x1B[38;2;248;248;242m \x1B[0m\x1B[38;2;230;219;116mbar\x1B[0m",
-			Limit:         7,
-			KeepNewlines:  true,
-			PreserveSpace: false,
-			TabWidth:      0,
-		},
-		// ANSI control codes don't get wrapped:
-		{
-			Input:         "\x1B[38;2;249;38;114m(\x1B[0m\x1B[38;2;248;248;242mjust another test\x1B[38;2;249;38;114m)\x1B[0m",
-			Expected:      "\x1B[38;2;249;38;114m(\x1B[0m\x1B[38;2;248;248;242mju\nst \nano\nthe\nr t\nest\x1B[38;2;249;38;114m\n)\x1B[0m",
-			Limit:         3,
-			KeepNewlines:  true,
-			PreserveSpace: false,
-			TabWidth:      0,
-		},
-	}
+var tt = []struct {
+	Input         string
+	Expected      string
+	Limit         int
+	KeepNewlines  bool
+	PreserveSpace bool
+	TabWidth      int
+}{
+	// No-op, should pass through, including trailing whitespace:
+	{
+		Input:         "foobar\n ",
+		Expected:      "foobar\n ",
+		Limit:         0,
+		KeepNewlines:  true,
+		PreserveSpace: false,
+		TabWidth:      0,
+	},
+	// Nothing to wrap here, should pass through:
+	{
+		Input:         "foo",
+		Expected:      "foo",
+		Limit:         4,
+		KeepNewlines:  true,
+		PreserveSpace: false,
+		TabWidth:      0,
+	},
+	// In contrast to wordwrap we break a long word to obey the given limit
+	{
+		Input:         "foobarfoo",
+		Expected:      "foob\narfo\no",
+		Limit:         4,
+		KeepNewlines:  true,
+		PreserveSpace: false,
+		TabWidth:      0,
+	},
+	// Newlines in the input are respected if desired
+	{
+		Input:         "f\no\nobar",
+		Expected:      "f\no\noba\nr",
+		Limit:         3,
+		KeepNewlines:  true,
+		PreserveSpace: false,
+		TabWidth:      0,
+	},
+	// Newlines in the input can be ignored if desired
+	{
+		Input:         "f\no\nobar",
+		Expected:      "foo\nbar",
+		Limit:         3,
+		KeepNewlines:  false,
+		PreserveSpace: false,
+		TabWidth:      0,
+	},
+	// Leading whitespaces after forceful line break can be preserved if desired
+	{
+		Input:         "foo bar\n  baz",
+		Expected:      "foo\n ba\nr\n  b\naz",
+		Limit:         3,
+		KeepNewlines:  true,
+		PreserveSpace: true,
+		TabWidth:      0,
+	},
+	// Leading whitespaces after forceful line break can be removed if desired
+	{
+		Input:         "foo bar\n  baz",
+		Expected:      "foo\nbar\n  b\naz",
+		Limit:         3,
+		KeepNewlines:  true,
+		PreserveSpace: false,
+		TabWidth:      0,
+	},
+	// Tabs are broken up according to the configured TabWidth
+	{
+		Input:         "foo\tbar",
+		Expected:      "foo \n  ba\nr",
+		Limit:         4,
+		KeepNewlines:  true,
+		PreserveSpace: true,
+		TabWidth:      3,
+	},
+	// Remaining width of wrapped tab is ignored when space is not preserved
+	{
+		Input:         "foo\tbar",
+		Expected:      "foo \nbar",
+		Limit:         4,
+		KeepNewlines:  true,
+		PreserveSpace: false,
+		TabWidth:      3,
+	},
+	// ANSI sequence codes don't affect length calculation:
+	{
+		Input:         "\x1B[38;2;249;38;114mfoo\x1B[0m\x1B[38;2;248;248;242m \x1B[0m\x1B[38;2;230;219;116mbar\x1B[0m",
+		Expected:      "\x1B[38;2;249;38;114mfoo\x1B[0m\x1B[38;2;248;248;242m \x1B[0m\x1B[38;2;230;219;116mbar\x1B[0m",
+		Limit:         7,
+		KeepNewlines:  true,
+		PreserveSpace: false,
+		TabWidth:      0,
+	},
+	// ANSI control codes don't get wrapped:
+	{
+		Input:         "\x1B[38;2;249;38;114m(\x1B[0m\x1B[38;2;248;248;242mjust another test\x1B[38;2;249;38;114m)\x1B[0m",
+		Expected:      "\x1B[38;2;249;38;114m(\x1B[0m\x1B[38;2;248;248;242mju\nst \nano\nthe\nr t\nest\x1B[38;2;249;38;114m\n)\x1B[0m",
+		Limit:         3,
+		KeepNewlines:  true,
+		PreserveSpace: false,
+		TabWidth:      0,
+	},
+}
 
+func stripAnsi(input string) string {
+	state := statemachine.StateMachine{}
+	var out []byte
+	for _, b := range []byte(input) {
+		step := state.Next(b)
+		if step.IsPrinting() {
+			out = append(out, b)
+		}
+	}
+	return string(out)
+}
+
+func TestWrap(t *testing.T) {
 	for i, tc := range tt {
 		f := NewWriter(tc.Limit)
 		f.KeepNewlines = tc.KeepNewlines
@@ -128,6 +144,95 @@ func TestWrap(t *testing.T) {
 
 		if f.String() != tc.Expected {
 			t.Errorf("Test %d, expected:\n\n`%s`\n\nActual Output:\n\n`%s`", i, tc.Expected, f.String())
+		} else {
+			// check height writer with WriteString
+			h1 := NewHeightWriter(tc.Limit)
+			h1.KeepNewlines = tc.KeepNewlines
+			h1.PreserveSpace = tc.PreserveSpace
+			h1.TabWidth = tc.TabWidth
+			h1.WriteString(tc.Input)
+			if realHeight := strings.Count(stripAnsi(tc.Expected), "\n") + 1; realHeight != h1.Height() {
+				t.Errorf("Test %d, WriteString(%s) expected height %d, got %d", i, strconv.Quote(tc.Input), realHeight, h1.Height())
+			}
+
+			h2 := NewHeightWriter(tc.Limit)
+			h2.KeepNewlines = tc.KeepNewlines
+			h2.PreserveSpace = tc.PreserveSpace
+			h2.TabWidth = tc.TabWidth
+			h2.Write([]byte(tc.Input))
+			if realHeight := strings.Count(stripAnsi(tc.Expected), "\n") + 1; realHeight != h2.Height() {
+				t.Errorf("Test %d, Write(%s) expected height %d, got %d", i, strconv.Quote(tc.Input), realHeight, h2.Height())
+			}
+		}
+	}
+}
+
+func FuzzWrapHeightMatchesOriginal(f *testing.F) {
+	for _, tc := range tt {
+		f.Add(
+			tc.Input, tc.Limit, tc.KeepNewlines, tc.PreserveSpace, tc.TabWidth,
+		)
+	}
+	f.Fuzz(func(t *testing.T,
+		input string,
+		limit int,
+		keepNewlines bool,
+		preserveSpace bool,
+		tabWidth int) {
+
+		w := NewWriter(limit)
+		w.KeepNewlines = keepNewlines
+		w.PreserveSpace = preserveSpace
+		w.TabWidth = tabWidth
+		_, err := w.Write([]byte(input))
+		if err != nil {
+			return
+		}
+
+		hw := NewHeightWriter(limit)
+		hw.KeepNewlines = keepNewlines
+		hw.PreserveSpace = preserveSpace
+		hw.TabWidth = tabWidth
+		hw.WriteString(input)
+
+		result := w.String()
+		realHeight := strings.Count(stripAnsi(result), "\n") + 1
+		if realHeight != hw.Height() {
+			t.Errorf("WriteString(string) expected height %d, got %d (output: %s, stripped: %s)", realHeight, hw.Height(), strconv.Quote(result), strconv.Quote(stripAnsi(result)))
+		}
+
+		hw2 := NewHeightWriter(limit)
+		hw2.KeepNewlines = keepNewlines
+		hw2.PreserveSpace = preserveSpace
+		hw2.TabWidth = tabWidth
+		hw2.Write([]byte(input))
+
+		if realHeight != hw2.Height() {
+			t.Errorf("Write([]byte) expected height %d, got %d (output: %s, stripped: %s)", realHeight, hw2.Height(), strconv.Quote(result), strconv.Quote(stripAnsi(result)))
+		}
+	})
+}
+
+func BenchmarkWrapHeight(b *testing.B) {
+	for _, tc := range tt {
+		for i := 0; i < b.N; i++ {
+			t := NewHeightWriter(tc.Limit)
+			t.KeepNewlines = tc.KeepNewlines
+			t.PreserveSpace = tc.PreserveSpace
+			t.TabWidth = tc.TabWidth
+			t.WriteString(tc.Input)
+		}
+	}
+}
+
+func BenchmarkWrap(b *testing.B) {
+	for _, tc := range tt {
+		for i := 0; i < b.N; i++ {
+			t := NewWriter(tc.Limit)
+			t.KeepNewlines = tc.KeepNewlines
+			t.PreserveSpace = tc.PreserveSpace
+			t.TabWidth = tc.TabWidth
+			t.WriteString(tc.Input)
 		}
 	}
 }
